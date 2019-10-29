3da87ef13127d9230869cc4a8e1763b6877e133f
baewood <baewood@gmail.com>
2019-10-29 15:42:38 -0500

    no message

---
+++ FloatAndDouble/src/academy/learnprogramming/F_D.java
@@ -0,0 +1,25 @@
+// Lesson on float and double Primitive Types
+
+package academy.learnprogramming;
+
+public class F_D {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = (float) 5.25;
+        //isn't recommended. double is preferred. 5.25f is better to write
+        double myDoubleValue = 5d;
+
+    }
+}
