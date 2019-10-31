b73283fc11afe39a60922c0848587c959158b050
baewood <baewood@gmail.com>
2019-10-31 09:13:06 -0500

    no message

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,22 @@
+// Lesson on char and boolean Primitive Data Types
+
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D'; // character literal
+        char myUnicodeChar = '\u0044'; // Unicode for D
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBoolValue = true;
+        boolean myFalseBoolValue = false;
+
+        boolean isCustomerOverTwentyOne = true; // Just an example of how to write the syntax
+    }
+}
